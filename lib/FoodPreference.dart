fed15e70de9b97a0e3e58d0c6ee98564f4223523
bloedboemmel <jakob.jung@fau.de>
2023-03-05 20:29:07 +0100

    Small fixes for analysis Ready to submit

--- lib/FoodPreference.dart
+++ lib/FoodPreference.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 import 'package:PickyPal/product.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
--- lib/userSettings.dart
+++ lib/userSettings.dart
@@ -1,4 +1,6 @@
 
+// ignore_for_file: non_constant_identifier_names, file_names
+
 import 'package:PickyPal/FoodPreference.dart';
 import 'package:flutter/material.dart';
 import 'package:settings_ui/settings_ui.dart';
