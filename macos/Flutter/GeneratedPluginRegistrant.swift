d7ddd28c42e819cc8f224c1bd52bb8ecfb6c92be
Jakob Jung <bloedboemmel@gmail.com>
2024-09-30 11:11:29 +0200

    Update build.gradle, GeneratedPluginRegistrant.swift, and pubspec.yaml

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,7 @@
 import FlutterMacOS
 import Foundation
 
-import device_info_plus_macos
+import device_info_plus
 import firebase_core
 import firebase_crashlytics
 import shared_preferences_foundation
